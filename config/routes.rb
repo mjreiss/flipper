e6cc78066ef5bed71ad57af43586455750ba9108
Mike <mjreiss4@gmail.com>
2015-04-16 23:29:50 -0400

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post  'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 
 
   # Example of regular route:
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,18 @@ User.create!( name:  "Mike Reiss",
               email: "mike@example.com",
               password:              "foobar",
               password_confirmation: "foobar",
-              admin: true)
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@example.com"
   password = "foobar"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+  User.create!( name:  name,
+                email: email,
+                password:              password,
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now)
 end
